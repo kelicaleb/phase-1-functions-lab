258faea647495cf6ef25ffbc8fe4b76495edf775
kelicaleb <kelicaleb7@gmail.com>
2022-08-22 03:25:54 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(somevalue){
+    if(somevalue == 43){
+        console.log("The number of blocks id " + 1)
+    }
+    else if(somevalue == 50){
+        console.log("The number of blocks is " + 8)
+
+    }
+    else if(somevalue == 34){
+        console.log("The number of blocks is " + 8)
+
+    }
+   else{
+    console.log("No value has been passed ")
+   }
+}
+function distanceFromHqInFeet(somevalue){
+    if(start == 43){
+        console.log("distance is equal to " + 254)
+    }
+    else if(somevalue == 50 ){
+        console.log("distance is equal to " + 2112)
+    }
+    else if(somevalue == 34){
+        console.log("distance is equal to " + 2112)
+
+    }
+
+}
+function distanceTravelledInFeet(start, destination){
+    if(start == 43 && destination == 48){
+        console.log("distance is equal to " + 1320)
+    }
+    else if (start == 50 && destination == 60 ){
+        console.log("distance is equal to " + 2640)
+    }
+    else if (start ==34 && destination ==28 ){
+        console.log("distance is equal to " + 1584)
+    }
+
+
+}
+function calculatesFarePrice(start, destination){
+    if(start == 43 && destination == 44){
+        console.log("amount to pay " + 0 + "you are awarded a free sample ") 
+    }
+    else if(start == 50 && destination == 58){
+        console.log("amount to pay is equal; " + 25)
+    }
+    else if(start == 34 && destination == 24){
+        console.log("Cannot travel that far" )
+    }
+
+}
